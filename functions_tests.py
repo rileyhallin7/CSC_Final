f99fef3ff03276ac61adfd7e3f2ec213ca35715c
rileyhallin7 <rhallin@calpoly.edu>
2026-03-09 18:07:17 -0700

    Test Cases Update

--- functions_tests.py
+++ functions_tests.py
@@ -7,19 +7,24 @@ class TestCases(unittest.TestCase):
     def test_summarize1(self):
         input1 = 1937
         input2 = 1942
-        expected = 'Largest Population Year: 1940'
-        result = summarize(input1, input2)['highest']['year']
+        expected = 1940
+        result = summarize(input1, input2)['highest_year']
         self.assertEqual(result, expected)
     def test_summarize2(self):
         input1 = 1937
         input2 = 1942
-        expected = 'Largest Population: 101985'
-        result = summarize(input1, input2)['highest']['population']
+        expected = 101985
+        result = summarize(input1, input2)['highest_population']
         self.assertEqual(result, expected)
     def test_summarize3(self):
         input1 = 1937
         input2 = 1942
-        expected = {'highest': {'year': 'Largest Population Year: 1940', 'population': 'Largest Population: 101985'}, 'lowest': {'year': 'Smallest Population Year: 1937', 'population': 'Smallest Population: 35217'}, 'total_change': 'Total Change: 20941', 'percent_change': 'Percent Change: 59.462759462759465'}
+        expected = {'highest_population': 101985,
+                    'highest_year': 1940,
+                    'lowest_population': 35217,
+                    'lowest_year': 1937,
+                    'percent_change': 59.462759462759465,
+                    'total_change': 20941}
         result = summarize(input1, input2)
         self.assertEqual(result, expected)
 
